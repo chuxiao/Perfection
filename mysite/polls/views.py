d2f840cdd22c9cf375bafdebe4c499c74332339d
chuxiao <yanzengyan99@gmail.com>
2011-07-26 22:16:34 +0800

    完成URL 映射，实现最基本的视图代码。
    视图代码没有实质性东西，只是测试URL 映射确实能够工作。

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World! You are at the poll index.')
+
+
+def detail(request, poll_id):
+    return HttpResponse('You are looking at poll %s.' % poll_id)
+
+
+def results(request, poll_id):
+    return HttpResponse('You are looking at the results of poll %s.' % poll_id)
+
+
+def vote(request, poll_id):
+    return HttpResponse('You are voting on poll %s.' % poll_id)
