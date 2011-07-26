8ba7dcf22802603e92dec3bd5f6c3ad82bdb7438
chuxiao <yanzengyan99@gmail.com>
2011-07-26 22:35:33 +0800

    index 视图返回数据
    只是最基本的返回数据，没有良好的视图。

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,11 @@
+from polls.models import Poll
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello World! You are at the poll index.')
-
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
 
 def detail(request, poll_id):
     return HttpResponse('You are looking at poll %s.' % poll_id)
